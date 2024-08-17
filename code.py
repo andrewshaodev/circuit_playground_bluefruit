5db085f5d1bd67e70d21862c134ee2d7d023c38f
Andrew Shao <132319777+andrewshaodev@users.noreply.github.com>
2024-08-17 16:21:32 -0400

    Update code.py

--- code.py
+++ code.py
@@ -5,6 +5,29 @@ from audiopwmio import PWMAudioOut as AudioOut
 from adafruit_led_animation.color import RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, GOLD, \
     PINK, AQUA, JADE, AMBER, OLD_LACE, WHITE, BLACK
 
+light_position = -1
+
+def play_sound(filename):
+    with open("drumSounds/" + filename, "rb") as wave_file:
+        wave = WaveFile(wave_file)
+        audio.play(wave)
+        while audio.playing:
+            pass
+
+def move_dot(position, direction, color):
+    if color == RED:
+        filename = "drum_cowbell.wav"
+    else:
+        filename = "scratch.wav"
+    play_sound(filename)
+    pixels[position] = BLACK
+    position = position + direction
+    position = 9 if position < 0 else position
+    position = position % 10
+    pixels[position] = color
+    return position
+
+
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
 
 colors = [RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, GOLD,
@@ -22,14 +45,12 @@ speaker = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
 speaker.direction = digitalio.Direction.OUTPUT
 speaker.value = True
 
+audio = AudioOut(board.SPEAKER)
+
 while True:
     button_A.update()
     button_B.update()
     if button_A.pressed:
-        pixels.fill(BLUE)
-    elif button_A.released:
-        pixels.fill(BLACK)
+        light_position = move_dot(light_position, 1, RED)
     elif button_B.pressed:
-        pixels.fill(RED)
-    elif button_B.released:
-        pixels.fill(BLACK)
+        light_position = move_dot(light_position, -1, BLUE)
